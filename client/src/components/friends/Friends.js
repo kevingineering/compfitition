35f4465c2d5c38732d1831edffc64e81d33339be
kcharders <kcharders@gmail.com>
2020-01-27 17:30:26 -0600

    started implementing friends

--- client/src/components/friends/Friends.js
+++ client/src/components/friends/Friends.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import FriendContext from '../../contexts/friends/friendContext';
+import FriendItem from './FriendItem';
 
 const Friends = () => {
   return (
-    <ul className='collection with-header'>
+    <ul className='collection'>
       <li className='collection-header'><h5>Friends!</h5></li>
       {friends === null ? <h4>Loading...</h4> : (
           friends.map(friend => <FriendItem friend={friend} key={friend.id} />)
---
+++ client/src/contexts/friends/friendState.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import axios from 'axios';
+import FriendContext from './friendContext';
+import FriendReducer from './friendReducer';
+import
+
+const FriendState = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FriendState
