1b28c0ff4341b91dcbf1bf3379199514edd8b63c
kcharders <kcharders@gmail.com>
2020-03-21 12:34:25 -0500

    modified server to test pipeline

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const path = require('path')
-const connectDB = require('./dbConnection')
+//const connectDB = require('./dbConnection')
 const routes = require('./backend/routes/index')
 
 //initialize express object 
 const app = express()
 
 //connect to DB
-connectDB()
+//connectDB()
 
 //adds json body parser to our express object - calls next for us, so we move to next middleware after request body is parsed, automatically creates and populates req.body
-app.use(express.json({ extended: false }))
+app.use(express.json({ extended: true }))
 
 //all routes in route folder
-app.use(routes)
+//app.use(routes)
 
 //serve static assets (build folder) if in production
 if(process.env.NODE_ENV === 'production') {
@@ -25,7 +25,7 @@ if(process.env.NODE_ENV === 'production') {
   })
 } 
 else {
-  app.get('*', (req, res) => {
+  app.use('*', (req, res) => {
     res.status(501).json({msg: 'Server is not in production'})
   })
 }
